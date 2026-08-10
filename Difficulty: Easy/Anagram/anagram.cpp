c5188b5b52e3d08f1834a93380fc9777e13df97c
Abhay Singh <227230915+Abhay71044@users.noreply.github.com>
2026-08-10 15:00:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Anagram/anagram.cpp
+++ Difficulty: Easy/Anagram/anagram.cpp
@@ -1,11 +1,17 @@
 class Solution {
   public:
     bool areAnagrams(string& s1, string& s2) {
-        if(s1.length() != s2.length()) return false;
-        sort(s1.begin(),s1.end());
-        sort(s2.begin(),s2.end());
+        int arr[256]={0};
         for(int i=0;i<s1.length();i++){
-            if(s1[i] != s2[i]) return false;
+            arr[s1[i]]++;
+        }
+        for(int j=0;j<s2.length();j++){
+            arr[s2[j]]--;
+        }
+        for(int i=0;i<256;i++){
+            if(arr[i] != 0){
+                return false;
+            }
         }
         return true;
     }

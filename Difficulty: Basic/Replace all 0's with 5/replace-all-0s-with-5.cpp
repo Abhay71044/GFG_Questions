b65a7ec0569b00bb80f72caf70637bf19240c250
Abhay Singh <227230915+Abhay71044@users.noreply.github.com>
2026-08-21 23:39:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Replace all 0's with 5/replace-all-0s-with-5.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int convertFive(int n) {
+        string s=to_string(n);
+        for(int i=0;i<s.size();i++){
+            if(s[i] == '0'){
+                s[i]='5';
+            }
+        }
+        n=stoi(s);
+        return n;
+    }
+};

80f78dd626971008ad9b5aea690bb0bb193d9095
Abhay Singh <227230915+Abhay71044@users.noreply.github.com>
2026-08-29 13:40:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/N-Queen Problem/nqueen-problem.cpp
+++ Difficulty: Hard/N-Queen Problem/nqueen-problem.cpp
@@ -1,66 +1,63 @@
 class Solution {
   public:
   
-  bool issafe(vector<vector<char>>&board,int col,int row,int n){
-      int i=row;
-      int j=col;
-      //check row
-      while(j>=0){
-          if(board[i][j]=='Q'){
-              return false;
-          }
-          j--;
-      }
-      i=row;
-      j=col;
-      //check upper diagonal
-      while(i>=0 && j>=0){
-          if(board[i][j]=='Q'){
-              return false;
-          }
-          i--;
-          j--;
-      }
-      i=row;
-      j=col;
-      //check lowerdiagonal
-      while(i<n && j>=0){
-          if(board[i][j]=='Q'){
-              return false;
-          }
-          i++;
-          j--;
-      }
-      return true;
-  }
-
-  void solve(vector<vector<char>>&board,int col,int n,vector<vector<int>>&ans){
-      if(col>=n){
-          vector<int>temp;
-          for(int i=0;i<n;i++){
-              for(int j=0;j<n;j++){
-                  if(board[i][j] == 'Q')
+    bool issafe(vector<vector<char>>&board,int col,int row,int n){
+        int i=row;
+        int j=col;
+        //check col
+        while(j>=0){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+            j--;
+        }
+        i=row;
+        j=col;
+        //upper diagonal
+        while(i>=0 && j>=0){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+            i--;j--;
+        }
+        i=row;
+        j=col;
+        while(i<n && j>=0){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+            j--;
+            i++;
+        }
+        return true;
+    }
+  
+    void solve(vector<vector<char>>&board,vector<vector<int>>&ans,int col,int n){
+        if(col>=n){
+            vector<int>temp;
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(board[i][j]=='Q'){
                         temp.push_back(j+1);
-              }
-          }
-          ans.push_back(temp);
-          return;
-      }
-
-      for(int row=0;row<n;row++){
-          if(issafe(board,col,row,n)){
-              board[row][col]='Q';
-              solve(board,col+1,n,ans);
-              board[row][col]='.';
-          }
-      }
-  }
+                    }
+                }
+            }
+            ans.push_back(temp);
+        }
+        for(int row=0;row<n;row++){
+            if(issafe(board,col,row,n)){
+                board[row][col]='Q';
+                solve(board,ans,col+1,n);
+                board[row][col]='.';
+            }
+        }
+    }
   
     vector<vector<int>> nQueen(int n) {
         vector<vector<char>>board(n,vector<char>(n,'.'));
         int col=0;
         vector<vector<int>>ans;
-        solve(board,col,n,ans);
+        solve(board,ans,col,n);
         return ans;
     }
 };

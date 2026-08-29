07d7738869a40642c9a0dc07c325c40dad1ce4a3
Abhay Singh <227230915+Abhay71044@users.noreply.github.com>
2026-08-29 16:04:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Two Repeated Elements/two-repeated-elements.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    vector<int> twoRepeated(vector<int>& arr) {
+        int n = arr.size() - 2;
+        vector<int> freq(n+1,0);
+        vector<int> ans;
+
+        for(int i =0; i<arr.size(); i++)
+        {
+            freq[arr[i]]++;
+
+            if (freq[arr[i]] == 2){
+                ans.push_back(arr[i]);
+            }
+        }
+        return ans;
+    }
+};

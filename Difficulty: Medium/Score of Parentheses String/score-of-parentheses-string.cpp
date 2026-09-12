42cb1a6a7a2019aeba76403ec5b717846337859b
Abhay Singh <227230915+Abhay71044@users.noreply.github.com>
2026-09-12 16:12:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Score of Parentheses String/score-of-parentheses-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int scoreOfParentheses(string& s) {
+        stack<int>st;
+        st.push(0);
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                st.push(0);
+            }
+            else{
+                int val=st.top();
+                st.pop();
+                int score;
+                if(val==0) score=1;
+                else score=2*val;
+                st.top()+=score;
+            }
+        }
+        return st.top();
+    }
+};

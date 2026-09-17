6c07df5fa171c2afd86c6aaea33677ecd81fe669
Abhay Singh <227230915+Abhay71044@users.noreply.github.com>
2026-09-18 04:45:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reverse Words/reverse-words.cpp
+++ Difficulty: Easy/Reverse Words/reverse-words.cpp
@@ -1,23 +1,23 @@
 class Solution {
   public:
     string reverseWords(string &s) {
-        string ans="";
+        string result="";
         int i=s.size()-1;
         while(i>=0){
-            while(i>=0 && s[i] == '.'){
+            while(i>=0 && s[i]=='.'){
                 i--;
             }
             if(i<0) break;
             int end=i;
-            while(i>=0 && s[i] != '.'){
+            while(i>=0 && s[i]!='.'){
                 i--;
             }
             string word=s.substr(i+1,end-i);
-            if(!ans.empty()){
-                ans=ans+'.';
+            if(!result.empty()){
+                result+='.';
             }
-            ans=ans+word;
+            result=result+word;
         }
-        return ans;
+        return result;
     }
 };
